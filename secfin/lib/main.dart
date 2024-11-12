16896b9957e6b6f55dda0e44116dd775a5c6d966
Daniel Santana <daniel_santana_87@hotmail.com>
2024-11-12 09:57:32 -0300

    removendo debug

--- secfin/lib/main.dart
+++ secfin/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,  // Remove o banner de debug
       home: DespesasScreen(),
     );
   }
